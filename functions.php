66b313a76b346747c5176cf12c69d5f53b464cc5
Michael Dinerstein <md@missionmike.dev>
2026-02-04 19:06:12 -0700

    feat: setup dev support.

--- functions.php
+++ functions.php
@@ -1,27 +1,29 @@
 <?php
+
 /**
  * Pierian Child Theme functions and definitions
  *
  * @package Pierian_Child
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 /**
  * Enqueue parent and child theme styles.
  */
-function pierian_child_enqueue_styles() {
+function pierian_child_enqueue_styles()
+{
 	wp_enqueue_style(
 		'pierian-style',
 		get_template_directory_uri() . '/style.css',
 		array(),
-		wp_get_theme( 'pierian' )->get( 'Version' )
+		wp_get_theme('pierian')->get('Version')
 	);
 	wp_enqueue_style(
 		'pierian-child-style',
 		get_stylesheet_uri(),
-		array( 'pierian-style' ),
-		wp_get_theme()->get( 'Version' )
+		array('pierian-style'),
+		wp_get_theme()->get('Version')
 	);
 }
-add_action( 'wp_enqueue_scripts', 'pierian_child_enqueue_styles' );
+add_action('wp_enqueue_scripts', 'pierian_child_enqueue_styles');
