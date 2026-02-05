1ab5b4ad9b56b26f656f0149dc012268766657ef
Michael Dinerstein <md@missionmike.dev>
2026-02-04 19:13:07 -0700

    feat: cache busting.

--- functions.php
+++ functions.php
@@ -13,17 +13,19 @@ defined('ABSPATH') || exit;
  */
 function pierian_child_enqueue_styles()
 {
+	$version = '1.0.0';
+
 	wp_enqueue_style(
 		'pierian-style',
 		get_template_directory_uri() . '/style.css',
 		array(),
-		wp_get_theme('pierian')->get('Version')
+		$version
 	);
 	wp_enqueue_style(
 		'pierian-child-style',
 		get_stylesheet_uri(),
 		array('pierian-style'),
-		wp_get_theme()->get('Version')
+		$version
 	);
 }
 add_action('wp_enqueue_scripts', 'pierian_child_enqueue_styles');
