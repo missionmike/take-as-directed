360144deb90e31214c4874c6ee70a33ae4dcb608
Michael Dinerstein <md@missionmike.dev>
2026-02-04 19:07:06 -0700

    feat: add inline style overrides.

--- functions.php
+++ functions.php
@@ -27,3 +27,23 @@ function pierian_child_enqueue_styles()
 	);
 }
 add_action('wp_enqueue_scripts', 'pierian_child_enqueue_styles');
+
+
+add_action('wp_head', 'take_as_directed_custom_styles_inline');
+function take_as_directed_custom_styles_inline()
+{
+	echo '<style type="text/css">
+        .wp-block-post-content p {
+		  text-indent: 2rem;
+		}
+		
+		.shiftnav.shiftnav-skin-light ul.shiftnav-menu li.menu-item.current-menu-item > .shiftnav-target, 
+		.shiftnav.shiftnav-skin-light ul.shiftnav-menu li.menu-item ul.sub-menu .current-menu-item > .shiftnav-target {
+		  background-color: gray;
+		}
+		
+		.wp-block-navigation .wp-block-navigation-item.current-menu-item {
+		  background-color: lightgray;
+		}
+    </style>';
+}
